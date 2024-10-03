824cb9a83ed12a4758e2d5301f6d8e5a8c9a4bec
Victor Valenzuela <khancerberus@hotmail.cl>
2024-10-03 18:48:34 -0300

    feat: Add authentication support with auth-astro package

---
+++ auth.config.js
@@ -0,0 +1,12 @@
+// auth.config.ts
+import Twitch from '@auth/core/providers/twitch'
+import { defineConfig } from 'auth-astro'
+
+export default defineConfig({
+	providers: [
+		Twitch({
+			clientId: import.meta.env.TWITCH_CLIENT_ID,
+			clientSecret: import.meta.env.TWITCH_CLIENT_SECRET,
+		}),
+	],
+})
