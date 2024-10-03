a47f2c7c482a441f9b41602a6505e6424a0895ff
Victor Valenzuela <khancerberus@hotmail.cl>
2024-10-03 15:08:55 -0300

    chore: Add authentication support with auth-astro package

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from '@astrojs/tailwind';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), auth()]
 });
