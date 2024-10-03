d942cbf5263a300636bf9400db7f1f5782d91ba1
Victor Valenzuela <khancerberus@hotmail.cl>
2024-10-03 15:03:31 -0300

    chore: install vercel and tailwind adapters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind()]
+});
