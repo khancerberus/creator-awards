250e7d1c4a79eb7d60f7fe9b709477fdae8dad68
Victor Valenzuela <khancerberus@hotmail.cl>
2024-10-07 21:23:04 -0300

    chore: Install React integration for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import tailwind from '@astrojs/tailwind';
 
 import auth from 'auth-astro';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), auth()]
+  integrations: [tailwind(), auth(), react()]
 });
